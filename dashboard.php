d7b164d39e25a841dc4cf4ad931d80c6bc379daf
Subhashree-Bhuyan <subhashreebhuyan918@gmail.com>
2026-01-20 18:19:46 +0530

    Create dashboadrd page

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,26 @@
 <?php
-echo "this is dashboard page";
+session_start();
+if(!isset($_SESSION['admin'])){
+    header("Location: index.php");
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome, Admin</h1>
+
+    <ul>
+        <li>Patient Management</li>
+        <li>Doctor Management</li>
+        <li>Appointment Management</li>
+    </ul>
+
+    <a href="logout.php">Logout</a>
+</body>
+</html>
