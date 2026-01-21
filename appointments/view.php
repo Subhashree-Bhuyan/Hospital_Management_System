c72bbed2fc7dbd18ffea871817edfc6dd932a87f
Subhashree-Bhuyan <subhashreebhuyan918@gmail.com>
2026-01-21 15:00:31 +0530

    appointments(add, view) feature are completed

---
+++ appointments/view.php
@@ -0,0 +1,41 @@
+<?php
+include('../config/db.php');
+
+$q = "SELECT appointments.id,
+    patients.name AS patient_name,
+    doctors.name AS doctor_name, 
+    appointments.appointment_date 
+    FROM appointments 
+    JOIN patients ON appointments.patient_id = patients.patient_id
+    JOIN doctors ON appointments.doctor_id = doctors.doctor_id";
+
+    $res = mysqli_query($con, $q);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Appointments</h2>
+
+    <table border="1">
+        <tr>
+            <th>Patient</th>
+            <th>Doctor</th>
+            <th>Date</th>
+        </tr>
+
+        <?php while($row = mysqli_fetch_assoc($res)) { ?>
+        <tr>
+            <td><?= $row['patient_name'] ?></td>
+            <td><?= $row['doctor_name'] ?></td>
+            <td><?= $row['appointment_date'] ?></td>
+        </tr>
+<?php } ?>
+    </table>
+</body>
+</html>
